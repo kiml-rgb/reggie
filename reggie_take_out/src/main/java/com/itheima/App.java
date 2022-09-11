b0ad151d82740e4ad00e94bdc7ac96a5d79f8d5b
kiml-rbg <1162278448@qq.com>
2022-09-11 15:16:49 +0800

    初始化工程

---
+++ reggie_take_out/src/main/java/com/itheima/App.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyf
+ * @program: reggie_take_out
+ * @description:
+ * @date 2022-09-11 15:12:13
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
